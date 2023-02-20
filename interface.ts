a320a185bbc267a94ea4d139c8135ac1af5cba4c
Prottoy Ghosh <ghoshprottoy98@gmail.com>
2023-02-20 17:26:51 +0600

    8th commit

--- interface.ts
+++ interface.ts
@@ -12,7 +12,7 @@ let {name: userName, email:UserLogin}: User = {name: "John", id: 1, email: ""};
 
 interface Employees extends User{
     salary: number;
-}
+};
 
 
 let employee: Employees= {name: "John", id: 1, email: "", salary: 1000};
@@ -34,3 +34,4 @@ console.log(user2);
 console.log(restUsers);
 
 //let result = restUsers.filter(user => user.id > 3);
+
