585931cfc081ab10b9db6fa855f1c243ed7937bb
Prottoy Ghosh <ghoshprottoy98@gmail.com>
2023-02-20 16:07:19 +0600

    2nd commit

--- datatypes.js
+++ datatypes.js
@@ -3,25 +3,16 @@ let lname;
 lname = "Prottoy";
 let newname = lname.toUpperCase();
 console.log(newname);
-
 let empList;
 empList = ["We", "Will", "Rock"];
-
 let numlist;
 numlist = [1, 2, 3, 4, 5];
-
 let results = numlist.filter((num) => num > 2);
-
 let num = numlist.find((num) => num === 2);
-
 let sum = numlist.reduce((acc, num) => acc + num);
-
 console.log(results);
-
 console.log(num);
-
 console.log(sum);
-
 var Color;
 (function (Color) {
     Color[Color["Red"] = 0] = "Red";
@@ -29,3 +20,13 @@ var Color;
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {}));
 let c = Color.Blue;
+let swapNum;
+function swapNumbers(num1, num2) {
+    return [num2, num1];
+}
+swapNum = swapNumbers(10, 20);
+swapNum[0];
+swapNum[1];
+let department;
+department = "IT";
+department = 10;
